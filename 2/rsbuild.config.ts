dafb91e93b506a62cba84fd96e6badca98ef2405
RJiazhen <jiazhen.ruan@outlook.com>
2025-10-20 00:05:59 +0800

    feat: add support for no-minify builds and related scripts

--- 2/rsbuild.config.ts
+++ 2/rsbuild.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 
+/** @description 是否不压缩代码 */
+const isNoMinify = process.env.IS_NO_MINIFY === 'true';
+
 export default defineConfig({
   plugins: [pluginReact()],
   output: {
-    minify: false,
-  }
+    distPath: {
+      root: isNoMinify ? 'dist-no-minify' : 'dist-minify',
+    },
+    minify: !isNoMinify,
+  },
 });
